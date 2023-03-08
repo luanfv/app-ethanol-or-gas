512a82706ae09abe817ded3f655376fe7899ebd2
luanfv <luanfrancavieira@gmail.com>
2023-03-08 18:21:38 -0300

    chore: add screens

--- AppAlcoolOuGasolina/Calculator/CalculatorVC.swift
+++ AppAlcoolOuGasolina/Calculator/CalculatorVC.swift
@@ -8,22 +8,25 @@
 import UIKit
 
 class CalculatorVC: UIViewController {
+    var screen: CalculatorScreen?
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    override func loadView() {
+        screen = CalculatorScreen()
+        view = screen
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        screen?.delegate(delegate: self)
     }
-    */
+}
 
+extension CalculatorVC: CalculatorScreenDelegate {
+    func tappedCalculateButton() {
+        print(#function)        
+    }
+    
+    func tappedBackButton() {
+        print(#function)
+    }
 }
