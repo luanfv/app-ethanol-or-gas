e5754e857d6697968eaa905d783ecbd8dd7fe49f
luanfv <luanfrancavieira@gmail.com>
2023-03-08 18:21:38 -0300

    chore: add screens

--- AppAlcoolOuGasolina/Calculator/Screen/CalculatorScreen.swift
+++ AppAlcoolOuGasolina/Calculator/Screen/CalculatorScreen.swift
@@ -7,14 +7,138 @@
 
 import UIKit
 
+protocol CalculatorScreenDelegate: AnyObject {
+    func tappedCalculateButton()
+    func tappedBackButton()
+}
+
 class CalculatorScreen: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    private weak var delegate: CalculatorScreenDelegate?
+    
+    public func delegate(delegate: CalculatorScreenDelegate) {
+        self.delegate = delegate
     }
-    */
+    
+    lazy var backgroundImage: UIImageView = {
+       let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: "BG BLUR DARK.png")
+        image.contentMode = .scaleAspectFit
+        
+        return image
+    }()
+    
+    lazy var logoAppImageView: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: "LOGO menor")
+        image.contentMode = .scaleAspectFit
+        
+        return image
+    }()
+    
+    lazy var ethanolPriceTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.autocorrectionType = .no
+        textField.backgroundColor = .white
+        textField.borderStyle = .roundedRect
+        textField.keyboardType = .decimalPad
+        textField.placeholder = "Preço do Álcool"
+        textField.textColor = .darkGray
+        
+        return textField
+    }()
+    
+    lazy var gasPriceTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.autocorrectionType = .no
+        textField.backgroundColor = .white
+        textField.borderStyle = .roundedRect
+        textField.keyboardType = .decimalPad
+        textField.placeholder = "Preço da Gasolina"
+        textField.textColor = .darkGray
+        
+        return textField
+    }()
+    
+    lazy var calculateButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Calcular", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        button.setTitleColor(.white, for: .normal)
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 8
+        button.backgroundColor = UIColor(red: 128/255, green: 128/255, blue: 128/255, alpha: 1.0)
+        button.addTarget(self, action: #selector(tappedCalculateButton), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    @objc func tappedCalculateButton() {
+        delegate?.tappedCalculateButton()
+    }
+    
+    lazy var backButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named: "Botão Back"), for: .normal)
+        button.addTarget(self, action: #selector(tappedBackButton), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    @objc func tappedBackButton() {
+        delegate?.tappedBackButton()
+    }
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(backgroundImage)
+        addSubview(logoAppImageView)
+        addSubview(ethanolPriceTextField)
+        addSubview(gasPriceTextField)
+        addSubview(calculateButton)
+        addSubview(backButton)
+        
+        configConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configConstraints() {
+        NSLayoutConstraint.activate([
+            backgroundImage.topAnchor.constraint(equalTo: topAnchor),
+            backgroundImage.leadingAnchor.constraint(equalTo: leadingAnchor),
+            backgroundImage.trailingAnchor.constraint(equalTo: trailingAnchor),
+            backgroundImage.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            backButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            backButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            
+            logoAppImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 150),
+            logoAppImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            logoAppImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            
+            ethanolPriceTextField.topAnchor.constraint(equalTo: logoAppImageView.bottomAnchor, constant: 134),
+            ethanolPriceTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            ethanolPriceTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            ethanolPriceTextField.heightAnchor.constraint(equalToConstant: 45),
+            
+            gasPriceTextField.topAnchor.constraint(equalTo: ethanolPriceTextField.bottomAnchor, constant: 16),
+            gasPriceTextField.leadingAnchor.constraint(equalTo: ethanolPriceTextField.leadingAnchor),
+            gasPriceTextField.trailingAnchor.constraint(equalTo: ethanolPriceTextField.trailingAnchor),
+            gasPriceTextField.heightAnchor.constraint(equalTo: ethanolPriceTextField.heightAnchor),
+            
+            calculateButton.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -135),
+            calculateButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 60),
+            calculateButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -60),
+            calculateButton.heightAnchor.constraint(equalToConstant: 44),
+        ])
+    }
 }
--- AppAlcoolOuGasolina/Home/HomeVC.swift
+++ AppAlcoolOuGasolina/Home/HomeVC.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class HomeVC: UIViewController {
+    var screen: HomeScreen?
+    
+    override func loadView() {
+        screen = HomeScreen()
+        view = screen
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        screen?.delegate(delegate: self)
     }
-
-
 }
 
+extension HomeVC: HomeScreenDelegate {
+    func tappedStartButton() {
+        let vc = CalculatorVC()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
