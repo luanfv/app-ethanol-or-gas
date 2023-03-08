762ef50a8a6f4cfed3cdfd8a2866dc2092eaf72f
luanfv <luanfrancavieira@gmail.com>
2023-03-08 15:32:51 -0300

    Initial Commit

---
+++ AppAlcoolOuGasolina/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppAlcoolOuGasolina
+//
+//  Created by Luan Franca Vieira on 08/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
